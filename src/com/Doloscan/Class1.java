0e7e6e21009ac90f7c2638350b5f8706bcca91b7
Dima <doloshkan96@mail.ru>
2019-12-18 12:36:25 +0200

    Commit

---
+++ src/com/Doloscan/Class1.java
@@ -0,0 +1,17 @@
+package com.Doloscan;
+
+public class Class1 {
+
+    static private int counter = 0;
+
+    static void count(){
+
+        counter ++;
+    }
+
+
+   static void method(){
+
+        System.out.println(counter);
+    }
+}
---
+++ src/com/Doloscan/Main.java
@@ -0,0 +1,19 @@
+package com.Doloscan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Class1 class1  = new Class1();
+
+      class1.count();
+      Class1.count();
+      class1.count();
+      Class1.count();
+
+      class1.method();
+
+
+    }
+}
